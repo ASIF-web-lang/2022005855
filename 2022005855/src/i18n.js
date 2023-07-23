4e8d98c44e47ef980ca8970b125cf69150990391
Asif Mohd <67969280+ASIF-web-lang@users.noreply.github.com>
2023-07-23 06:04:36 +0600

    i18n.js

--- 2022005855/src/i18n.js
+++ 2022005855/src/i18n.js
@@ -1,9 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// import translationEN from '../public/locales/en/translation.json';
-// import translationFI from '../public/locales/fi/translation.json';
-// import common_en from './translations/en/common.json';
-// import common_fi from './translations/fi/common.json';
 import translationEN from './translations/en/translation.json';
 import translationFI from './translations/fi/translation.json';
 
